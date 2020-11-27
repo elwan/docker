78d0eb2e296c6b0471211982c504dcac402db5a2
admin01 <admin01@flightstats>
2020-11-27 16:40:41 +0100

    dockerize flightstats_api

--- Flightstats_api/flightstats_api.py
+++ Flightstats_api/flightstats_api.py
@@ -70,4 +70,4 @@ class FlightList(Resource):
         return cedeaopax
 
 if __name__ == '__main__' :
-    app.run(debug=True,host= '0.0.0.0',port = 5080)
+    app.run(debug=True,host= '0.0.0.0',port = 5050)
